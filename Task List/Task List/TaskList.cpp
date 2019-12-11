f2fa15d10d226a93a00ea1f93c576dd218f5a0a1
Zachary Rich <ZachRichDev@gmail.com>
2019-12-10 19:14:58 -0500

    Progress

---
+++ Task List/Task List/TaskList.cpp
@@ -0,0 +1,32 @@
+#include <list>
+#include <iterator>
+#include <iostream>
+#include 'Task.h'
+
+using namespace std;
+
+class TaskList()
+{
+    Task::list<Task> taskList;
+    
+    TaskList getCompletedTasks()
+    {
+        
+    }
+    
+    TaskList getOngoingTasks()
+    {
+        
+    }
+    
+    TaskList getNotStartedTasks()
+    {
+        
+    }
+    
+    void Task::addTask(Task task, TaskList listName)
+    {
+        
+    }
+}
+ 
